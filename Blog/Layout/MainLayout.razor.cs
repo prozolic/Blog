e8e573d7f821be338bf92c1568a137a30a348d45
prozolic <42107886+prozolic@users.noreply.github.com>
2025-07-02 16:36:34 +0900

    upd

--- Blog/Layout/MainLayout.razor.cs
+++ Blog/Layout/MainLayout.razor.cs
@@ -7,6 +7,9 @@ public partial class MainLayout
     [Inject]
     public IConfiguration? Configuration { get; set; }
 
+    [Inject]
+    public NavigationManager? NavigationManager { get; set; }
+
     public string? Name { get; private set; }
 
     public string? Description { get; private set; }
@@ -14,6 +17,10 @@ public partial class MainLayout
     public string? GithubUrl { get; private set; }
 
     public string? BlogSourceUrl { get; private set; }
+
+    public Post? PreviousPost { get; private set; }
+
+    public Post? NextPost { get; private set; }
 
     protected override Task OnInitializedAsync()
     {
@@ -25,7 +32,41 @@ public partial class MainLayout
         GithubUrl = section?["GithubUrl"];
         BlogSourceUrl = section?["BlogSourceUrl"];
 
+        FindPreviousAndNextPosts();
+
         return Task.CompletedTask;
+    }
+
+    protected override Task OnParametersSetAsync()
+    {
+        FindPreviousAndNextPosts();
+        return base.OnParametersSetAsync();
+    }
+
+    private void FindPreviousAndNextPosts()
+    {
+        var currentUrl = NavigationManager?.ToBaseRelativePath(NavigationManager.Uri);
+        if (!string.IsNullOrEmpty(currentUrl) && !currentUrl.Equals("/"))
+        {
+            currentUrl = "/" + currentUrl;
+            FindPreviousAndNextPostsCore(currentUrl);
+        }
+    }
+
+    private void FindPreviousAndNextPostsCore(string currentUrl)
+    {
+        var currentPostIndex = 0;
+        foreach(var post in PostProvider.AllPosts.AsSpan())
+        {
+            if (post.Url == currentUrl)
+            {
+                break;
+            }
+            currentPostIndex++;
+        }
+
+        NextPost = currentPostIndex > 0 ? PostProvider.AllPosts[currentPostIndex - 1] : null;
+        PreviousPost = currentPostIndex < PostProvider.AllPosts.Length - 1 ? PostProvider.AllPosts[currentPostIndex + 1] : null;
     }
 
 }
